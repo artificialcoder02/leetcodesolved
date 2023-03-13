b135d483a13aaf57b088f9a0ab67eca653dda0ba
Tuhin Roychowdhury <97015839+artificialcoder02@users.noreply.github.com>
2023-03-13 23:18:39 +0530

    Create solution.cpp

---
+++ 101. Symmetric Tree/solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isSymmetricHelper(TreeNode* leftNode, TreeNode* rightNode){
+        if(leftNode==NULL && rightNode==NULL) return true;
+        if(leftNode==NULL || rightNode==NULL || leftNode->val != rightNode->val) return false;
+        
+        return isSymmetricHelper(leftNode->left, rightNode->right) && isSymmetricHelper(leftNode->right, rightNode->left);
+    }
+
+    bool isSymmetric(TreeNode* root) {
+        if(root == NULL) return true;
+        return isSymmetricHelper(root->left, root->right);
+    }
+};
