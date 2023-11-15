6021b4c629b4910174bc2695171a05bc2e62efb7
Tuhin Roychowdhury <97015839+artificialcoder02@users.noreply.github.com>
2023-11-15 23:39:39 +0530

    Create solution.py

---
+++ 1846. Maximum Element After Decreasing and Rearranging/solution.py
@@ -0,0 +1,8 @@
+class Solution:
+    def maximumElementAfterDecrementingAndRearranging(self, arr: List[int]) -> int:
+        arr.sort()
+        ans=1
+        for x in arr[1:]:
+            ans=min(x, ans+1)
+        return ans
+        
